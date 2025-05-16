e228c17487425e5ecf456c82e48d96f1af9464bf
TechWithYann <techwithyann@gmail.com>
2025-05-16 07:06:21 +0100

    fixed the issue with prisma, added tanstack query, added the dialog box

---
+++ actions/workflows/getWorkflowsForUser.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+
+export async function GetWorkflowsForUser() {
+  // We need to know the user ID and by doing that we are also check if the user is authenticated
+  const { userId } = await auth();
+
+  // If user id is not defined, throw an error
+  if (!userId) {
+    throw new Error("unauthenticated");
+  }
+
+  //  If everything is ok
+  return prisma.workflow.findMany({
+    // Getting all the workflows for a particular userId
+    where: {
+      userId,
+    },
+    // Put them in ascending order
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+}
---
+++ app/(dashboard)/workflows/_components/CreateWorkflowDialog.tsx
@@ -0,0 +1,28 @@
+// A folder name prefixed by '_' are private folders in nextjs meaning that folder and all sub folder will not be considered by the routing system
+"use client";
+
+import React, { useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Layers2Icon } from "lucide-react";
+import CustomDialogHeader from "@/components/CustomDialogHeader";
+
+function CreateWorkflowDialog({ triggerText }: { triggerText?: string }) {
+  const [open, setOpen] = useState(false);
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button>{triggerText ?? "Create workflow"}</Button>
+      </DialogTrigger>
+      <DialogContent className="px-0">
+        <CustomDialogHeader
+          icon={Layers2Icon}
+          title="Create workflow"
+          subTitle="Start building your workflow"
+        />
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default CreateWorkflowDialog;

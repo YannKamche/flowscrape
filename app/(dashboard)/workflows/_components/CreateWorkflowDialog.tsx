1a43cdd7dffc3655f0b3f1abc851aca23ea60544
YannKamche <yannkamche@gmail.com>
2025-05-17 11:30:20 +0100

    Added the form

--- app/(dashboard)/workflows/_components/CreateWorkflowDialog.tsx
+++ app/(dashboard)/workflows/_components/CreateWorkflowDialog.tsx
@@ -6,9 +6,32 @@ import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Layers2Icon } from "lucide-react";
 import CustomDialogHeader from "@/components/CustomDialogHeader";
+import { useForm } from "react-hook-form";
+import { createWorkflowSchema } from "@/schema/workflow";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 function CreateWorkflowDialog({ triggerText }: { triggerText?: string }) {
   const [open, setOpen] = useState(false);
+
+  // Definition of the form
+  const form = useForm<z.infer<typeof createWorkflowSchema>>({
+    resolver: zodResolver(createWorkflowSchema),
+    defaultValues: {},
+  });
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
@@ -20,6 +43,69 @@ function CreateWorkflowDialog({ triggerText }: { triggerText?: string }) {
           title="Create workflow"
           subTitle="Start building your workflow"
         />
+
+        {/* Define the form */}
+        <div className="p-6">
+          <Form {...form}>
+            <form className="space-y-8 w-full">
+              <FormField
+                // The control field is used by react hook form to control the form field
+                control={form.control}
+                // The name of the field
+                name="name"
+                // The render function renders the form item
+                render={({ field }) => (
+                  <FormItem>
+                    {/* Contains a form label with 'Name' within it */}
+                    <FormLabel className="flex gap-1 items-center">
+                      Name
+                      <p className="text-xs text-primary">(required)</p>
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Choose a descriptive and unique name
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                // The control field is used by react hook form to control the form field
+                control={form.control}
+                // The name of the field
+                name="description"
+                // The render function renders the form item
+                render={({ field }) => (
+                  <FormItem>
+                    {/* Contains a form label with 'Name' within it */}
+                    <FormLabel className="flex gap-1 items-center">
+                      Description
+                      <p className="text-xs text-muted-foreground">
+                        (optional)
+                      </p>
+                    </FormLabel>
+                    <FormControl>
+                      <Textarea {...field} className="resize-none" />
+                    </FormControl>
+                    <FormDescription>
+                      Provide a brief description of what your workflow does.{" "}
+                      <br />
+                      This is optional but can help you remember the
+                      workflow&apos;s purpose
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full">
+                Proceed
+              </Button>
+            </form>
+          </Form>
+        </div>
       </DialogContent>
     </Dialog>
   );
---
+++ schema/workflow.ts
@@ -0,0 +1,8 @@
+// Definition of the schema for the form with zod
+
+import { z } from "zod";
+
+export const createWorkflowSchema = z.object({
+  name: z.string().max(50),
+  description: z.string().max(80).optional(),
+});
